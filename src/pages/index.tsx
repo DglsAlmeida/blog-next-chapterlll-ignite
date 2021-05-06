ce8d830f08347f6acac1cb3b62c30e61250191bd
rivand <rivand@RIVAND1>
2021-05-05 23:31:01 -0300

    Finished styles of the home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { GetStaticProps } from 'next';
-
+import Head from 'next/head';
+import { FiUser, FiCalendar } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +26,101 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | Blog</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <div className={styles.posts}>
+          <img src="/images/logo.svg" alt="logo" />
+          <a href="/">
+            <h1>Como utilizar Hooks</h1>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <span>
+                <FiCalendar />
+                15 Mar 2021
+              </span>
+              <span>
+                <FiUser />
+                Joseph Oliveira
+              </span>
+            </div>
+          </a>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+          <a href="/">
+            <h1>Criando um app CRA do zero</h1>
+            <p>
+              Tudo sobre como criar a sua primeira aplicação utilizando Create
+              React App
+            </p>
+            <div>
+              <span>
+                <FiCalendar />
+                19 Abr 2021
+              </span>
+              <span>
+                <FiUser />
+                Danilo Vieira
+              </span>
+            </div>
+          </a>
 
-//   // TODO
+          <a href="/">
+            <h1>Como utilizar Hooks</h1>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <span>
+                <FiCalendar />
+                15 Mar 2021
+              </span>
+              <span>
+                <FiUser />
+                Joseph Oliveira
+              </span>
+            </div>
+          </a>
+
+          <a href="/">
+            <h1>Criando um app CRA do zero</h1>
+            <p>
+              Tudo sobre como criar a sua primeira aplicação utilizando Create
+              React App
+            </p>
+            <div>
+              <span>
+                <FiCalendar />
+                19 Abr 2021
+              </span>
+              <span>
+                <FiUser />
+                Danilo Vieira
+              </span>
+            </div>
+          </a>
+
+          <button type="button">Carregar mais posts</button>
+        </div>
+      </main>
+    </>
+  );
+}
+
+// export const getStaticProps: GetStaticProps = async () => {
+//   const prismic = getPrismicClient();
+//   const postsResponse = await prismic.query();
+
+//   if (!postsResponse) {
+//     return {
+//       notFound: true,
+//     }
+//   }
+
+//   return {
+//     props: {
+
+//     }
+//   }
 // };
